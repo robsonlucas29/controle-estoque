80c848f23922dae991a4c8005594f8787fbc936c
Robson Lucas <lucas2952001@gmail.com>
2026-05-18 17:13:04 -0300

    Corrige import React no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import logo from "./logo.png";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable";
 import * as XLSX from "xlsx";
